b862d09261ab9eaeef32844eaef7a2ed886ebeaa
Roverp <yar.zubaha@gmail.com>
2024-04-18 19:29:02 +0200

    Add some pointers

--- main.c
+++ main.c
@@ -22,18 +22,31 @@ typedef struct {
 
 typedef struct {
   TypeLiteral type; 
-  int value;
+  char *value;
 } TokenLiteral;
 
 typedef struct {
  TypeSeparator type; 
 } TokenSeparator;
 
-void generate_number(char current, FILE *file) {
-  while (current != EOF) {
-    printf("%c\n", current);
+TokenLiteral *generate_number(char current, FILE *file) {
+  // Stoped at 25:13
+  TokenLiteral *token = malloc(sizeof(TokenLiteral));
+  token->type = INT;
+  char *value = malloc(sizeof(char) * 8);
+  int value_index = 0;
+  while (isdigit(current) && current != EOF) {
+    if (!isdigit(current)) {
+      break;
+    }
+    value[value_index] = current;
+    value_index++;
+    printf("%c", current);
     current = fgetc(file);
   }
+  token->value = value;
+  free(value);
+  return (token);
 }
 
 void lexer(FILE *file) {
@@ -47,7 +60,8 @@ void lexer(FILE *file) {
     } else if (current == ')') {
       printf("FOUND CLOSE PAREN\n");
     } else if (isdigit(current)) {
-      generate_number(current, file);
+      TokenLiteral *test_token = generate_number(current, file);
+      printf("TEST TOKEN VALUE: %s\n", test_token->value);
       // printf("FOUND DIGIT: %d\n", current - '0');
     } else if (isalpha(current)) {
       printf("FOUND CHARACTER: %c\n", current);
