b0ff8c133986ae7f2623de5658b0a9b91660ed38
Roverp <yar.zubaha@gmail.com>
2024-04-18 18:48:05 +0200

    Add generate_number funciton

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 typedef enum {
   SEMI,
@@ -28,12 +29,28 @@ typedef struct {
  TypeSeparator type; 
 } TokenSeparator;
 
+void generate_number(char current, FILE *file) {
+  while (current != EOF) {
+    printf("%c\n", current);
+    current = fgetc(file);
+  }
+}
+
 void lexer(FILE *file) {
   char current = fgetc(file);
 
   while (current != EOF) {
     if (current == ';') {
       printf("FOUND SEMICOLON\n");
+    } else if (current == '(') {
+      printf("FOUND OPEN PAREN\n");
+    } else if (current == ')') {
+      printf("FOUND CLOSE PAREN\n");
+    } else if (isdigit(current)) {
+      generate_number(current, file);
+      // printf("FOUND DIGIT: %d\n", current - '0');
+    } else if (isalpha(current)) {
+      printf("FOUND CHARACTER: %c\n", current);
     }
 
     // printf("%c", current);
